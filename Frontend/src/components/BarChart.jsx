e86db44acda5d83cb2bc549a3938a56dd05ca088
jai <jai@pearlorganisation.com>
2024-07-24 17:41:25 +0530

    updates

--- Frontend/src/components/BarChart.jsx
+++ Frontend/src/components/BarChart.jsx
@@ -1,33 +1,68 @@
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import 'chart.js/auto';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import "chart.js/auto";
+import zoomPlugin from "chartjs-plugin-zoom";
 
 // Register the components with ChartJS
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  zoomPlugin
+);
 
-const BarChart = ({data}) => {
-  console.log('chart data', data)
+const BarChart = ({ data }) => {
   const options = {
     scales: {
       x: {
-        type: 'category', // Correct type for x-axis
+        type: "category", // Correct type for x-axis
         barPercentage: 0.5,
-        categoryPercentage: 0.5
+        categoryPercentage: 0.5,
       },
       y: {
-        beginAtZero: true
-      }
+        beginAtZero: true,
+      },
     },
     plugins: {
       legend: {
         display: true,
-        position: 'top'
-      }
-    }
+        position: "top",
+      },
+      zoom: {
+        pan: {
+          enabled: true,
+          mode: 'y',
+        },
+        zoom: {
+          animation: {
+            duration: 1000,
+            easing: 'easeOutCubic'
+          },
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: "xy",
+        },
+      },
+    },
   };
 
-  return <Bar data={data} options={options} />;
+  return <Bar data={data} options={options} className="min-h-[320px]" />;
 };
 
 export default BarChart;
